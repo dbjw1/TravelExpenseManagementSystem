61292ffe9ea899ddc435077b350b02f9e9d394b7
AN JAE WON <dkswodnjs3@gnu.ac.kr>
2022-03-14 19:34:53 +0900

    adapted X to TravelExpense

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -3,11 +3,11 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1. Add Students");
-		System.out.println("2. Delete Students");
-		System.out.println("3. Edit Students");
-		System.out.println("4. View Students");
-		System.out.println("5. Exit Students");
+		System.out.println("1. Add Expense");
+		System.out.println("2. Delete Expense");
+		System.out.println("3. Edit Expense");
+		System.out.println("4. View Expense");
+		System.out.println("5. Exit");
 	}
 
 }
