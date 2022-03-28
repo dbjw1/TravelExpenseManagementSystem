b2f31867eb738bad994000a4cb82195b44e09ed9
AN JAE WON <dkswodnjs3@gnu.ac.kr>
2022-03-28 14:16:45 +0900

    add method

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class MenuManager {
+public class MenuManager{
 	public static void main(String[] args) {
 		int num=0;
 		Scanner input = new Scanner(System.in);
@@ -10,25 +10,58 @@ public class MenuManager {
 			System.out.println("4. View Expense");
 			System.out.println("5. Show a Menu");
 			System.out.println("6. Exit");
-			System.out.println("Select one number between 1-6");
+			System.out.println("#Select one number between 1-6");
 			num=input.nextInt();
 			switch(num) {
 				case 1:
-					System.out.print("Date :");
-					int date=input.nextInt();
-					System.out.print("Expender : ");
-					String edr=input.next();
-					System.out.print("Amount : ");
-					int amt=input.nextInt();
-					System.out.print("Category : ");
-					String cat=input.next();
-					String des=input.nextLine();
-					System.out.print("Description : ");
-					des=input.nextLine();
+					addexpense();
 					break;
 				case 2:
-					
+					deleteexpense();
+					break;
+				case 3:
+					editexpense();
+					break;
+				case 4:
+					viewexpense();
+					break;
+				case 5:
+					showamenu();
+					break;
+				case 6:
+					exit();
+					break;
 			}
 		}
+	}
+	public static void addexpense(){
+		Scanner input = new Scanner(System.in);
+		System.out.print("Date(MMDD) :");
+		String date=input.next();
+		System.out.print("Expender : ");
+		String edr=input.next();
+		System.out.print("Amount : ");
+		int amt=input.nextInt();
+		System.out.print("Category : ");
+		String cat=input.next();
+		String des=input.nextLine();
+		System.out.print("Description : ");
+		des=input.nextLine();
+		System.out.println();
+	}
+	public static void deleteexpense(){
+		
+	}
+	public static void editexpense(){
+		
+	}
+	public static void viewexpense(){
+
+	}
+	public static void showamenu(){
+		
+	}
+	public static void exit(){
+		
 	}
 }
