b9f6397882ea70704a9f497fc6ec423514c6239c
AN JAE WON <dkswodnjs3@gnu.ac.kr>
2022-04-04 19:01:29 +0900

    add Expense class and ExpenseManager class

---
+++ src/Expense.java
@@ -0,0 +1,30 @@
+
+public class Expense {
+	int id;
+	String date;
+	String expender;
+	int amount;
+	String category;
+	String description;
+	public Expense(){
+	}
+	public Expense(int id,String date,String expender,int amount,String category,String description) {
+		this.id=id;
+		this.date=date;
+		this.expender=expender;
+		this.amount=amount;
+		this.category=category;
+		this.description=description;
+	}
+	public void printInfo() {
+		System.out.println("id : "+id);
+		System.out.println("date : "+date);
+		System.out.println("expender : "+expender);
+		System.out.println("amount : "+amount);
+		System.out.println("category :"+category);
+		System.out.println("description : "+description);
+	}
+	public void printId() {
+		System.out.println("now expense data id is : "+id);
+	}
+}
---
+++ src/ExpenseManager.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class ExpenseManager {
+	Expense expense;
+	Scanner input;
+	ExpenseManager(Scanner input){
+		this.input=input;
+	}
+	
+	public void addExpense(){
+		expense=new Expense();
+		Scanner input = new Scanner(System.in);
+		System.out.print("ID : ");
+		expense.id=input.nextInt();
+		System.out.print("Date(MMDD) :");
+		expense.date=input.next();
+		System.out.print("Expender : ");
+		expense.expender=input.next();
+		System.out.print("Amount : ");
+		expense.amount=input.nextInt();
+		System.out.print("Category : ");
+		expense.category=input.next();
+		String nl=input.nextLine();
+		System.out.print("Description : ");
+		expense.description=input.nextLine();
+		System.out.println();
+	}
+	public void deleteExpense(){
+		System.out.print("ID : ");
+		int id=input.nextInt();
+		if(expense ==null) {
+			System.out.println("no expense data");
+			return;
+		}
+		if(expense.id==id) {
+			expense=null;
+			System.out.println("expense data is deleted");
+		}
+	}
+	public void editExpense(){
+		System.out.print("ID : ");
+		int id=input.nextInt();
+		if(expense.id==id) {
+			System.out.println("expense data to be edited is "+id);
+		}
+	}
+	public void viewExpense(){
+		System.out.print("ID : ");
+		int id=input.nextInt();
+		if(expense.id==id) {
+			expense.printInfo();
+		}
+	}
+}
